23e6530ff55c3dad90a3e0c8781c5b8bb6acba37
BrunoDeye <bruno.rodrigues@deye.solar>
2023-09-18 16:33:35 -0300

    feat: forcedThem handler test

--- src/lib/components/theme-toggle.tsx
+++ src/lib/components/theme-toggle.tsx
@@ -6,13 +6,13 @@ import { useTheme } from 'next-themes';
 import { Button } from '@/lib/components/ui/button';
 
 export function ThemeToggle() {
-  const { setTheme, theme } = useTheme();
-
+  const { setTheme, theme, forcedTheme } = useTheme();
+  
   return (
     <Button
       variant="ghost"
       size="sm"
-      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+      onClick={() => setTheme(forcedTheme ? forcedTheme : theme === 'light' ? 'dark' : 'light')}
     >
       <Sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
