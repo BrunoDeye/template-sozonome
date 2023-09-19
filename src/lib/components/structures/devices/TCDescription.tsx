5f769520990090afe5d50311edc921c1a042eb2c
BrunoDeye <bruno.rodrigues@deye.solar>
2023-09-19 15:55:01 -0300

    fix: TC description to work on mobile

--- src/lib/components/structures/devices/TCDescription.tsx
+++ src/lib/components/structures/devices/TCDescription.tsx
@@ -4,23 +4,27 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from '@/lib/components/ui/tooltip';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/lib/components/ui/popover';
 import { LucideInfo } from 'lucide-react';
 
 const TCDescription = () => {
+  
   return (
-    <TooltipProvider>
-      <Tooltip delayDuration={0}>
-        <TooltipTrigger>
-          <LucideInfo height={18} />
-        </TooltipTrigger>
-        <TooltipContent className="w-60 text-justify">
-          <p>
-            O fator de correção é usado para compensar as perdas no sistema e
-            pode variar dentro da faixa de 0,87 a 0,94.
-          </p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <Popover>
+      <PopoverTrigger className='hover:bg-sky-500 hover:-translate-x-[1px] hover:-translate-y-[1px] hover:underline rounded-full'>
+        <LucideInfo height={18} width={18} />
+      </PopoverTrigger>
+      <PopoverContent className="w-60 text-justify p-3">
+        <p>
+          O fator de correção é usado para compensar as perdas no sistema e
+          pode variar dentro da faixa de 0,87 a 0,94.
+        </p>
+      </PopoverContent>
+    </Popover>
   );
 };
 
