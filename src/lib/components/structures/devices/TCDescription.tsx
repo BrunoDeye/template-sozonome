0742738d79c504f0e4c2d2b10bcca22d005dd9c9
BrunoDeye <bruno.rodrigues@deye.solar>
2023-09-19 16:04:46 -0300

    feat: add hover to FC popover

--- src/lib/components/structures/devices/TCDescription.tsx
+++ src/lib/components/structures/devices/TCDescription.tsx
@@ -1,3 +1,4 @@
+'use client';
 import {
   Tooltip,
   TooltipContent,
@@ -10,22 +11,46 @@ import {
   PopoverTrigger,
 } from '@/lib/components/ui/popover';
 import { LucideInfo } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
 const TCDescription = () => {
-  
-  return (
-    <Popover>
-      <PopoverTrigger className='hover:bg-sky-500 hover:-translate-x-[1px] hover:-translate-y-[1px] hover:underline rounded-full'>
+  const [open, setOpen] = useState(false);
+
+  const handleMouseEnter = () => {
+    setOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    setOpen(false);
+  };
+
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return isClient ? (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        className="rounded-full hover:-translate-x-[1px] hover:-translate-y-[1px] hover:bg-sky-200 dark:hover:bg-sky-500"
+      >
         <LucideInfo height={18} width={18} />
       </PopoverTrigger>
-      <PopoverContent className="w-60 text-justify p-3">
+      <PopoverContent
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        className="w-60 p-3 text-justify "
+      >
         <p>
-          O fator de correção é usado para compensar as perdas no sistema e
-          pode variar dentro da faixa de 0,87 a 0,94.
+          O fator de correção é usado para compensar as perdas no sistema e pode
+          variar dentro da faixa de 0,87 a 0,94.
         </p>
       </PopoverContent>
     </Popover>
-  );
+  ) : null;
 };
 
 export default TCDescription;
