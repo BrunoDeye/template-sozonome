1c16ad9a6f2c8c06bdb8790fa3a7dee9ec79bf29
BrunoDeye <bruno.rodrigues@deye.solar>
2023-09-22 15:14:56 -0300

    feat: test loading for routing

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,22 +1,28 @@
 
+import LoadingDeye from '@/lib/components/Loading';
 import { Skeleton } from '@/lib/components/ui/skeleton';
 
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
 
   return (
-    <div className="mx-auto flex w-[100vw] sm:w-[50vw] flex-col items-center gap-8 px-6">
-      <Skeleton className="h-[52px] w-[50vw] sm:w-[200px]" />
-      <Skeleton className="h-6 w-full" />
-      <Skeleton className="h-6 w-full" />
-      <Skeleton className="h-6 w-full" />
-      <Skeleton className="h-3 w-full" />
+    // <div className="mx-auto flex w-[100vw] sm:w-[50vw] flex-col items-center gap-8 px-6">
+    //   <Skeleton className="h-[52px] w-[50vw] sm:w-[200px]" />
+    //   <Skeleton className="h-6 w-full" />
+    //   <Skeleton className="h-6 w-full" />
+    //   <Skeleton className="h-6 w-full" />
+    //   <Skeleton className="h-3 w-full" />
 
-      <Skeleton className="h-[100px] w-[70vw] sm:w-[40vw] rounded-lg" />
-      <Skeleton className="h-[100px] w-[70vw] sm:w-[40vw] rounded-lg" />
-      <Skeleton className="h-[100px] w-[70vw] sm:w-[40vw] rounded-lg" />
-      <Skeleton className="h-[100px] w-[70vw] sm:w-[40vw] rounded-lg" />
-      <Skeleton className="h-[100px] w-[70vw] sm:w-[40vw] rounded-lg" />
+    //   <Skeleton className="h-[100px] w-[70vw] sm:w-[40vw] rounded-lg" />
+    //   <Skeleton className="h-[100px] w-[70vw] sm:w-[40vw] rounded-lg" />
+    //   <Skeleton className="h-[100px] w-[70vw] sm:w-[40vw] rounded-lg" />
+    //   <Skeleton className="h-[100px] w-[70vw] sm:w-[40vw] rounded-lg" />
+    //   <Skeleton className="h-[100px] w-[70vw] sm:w-[40vw] rounded-lg" />
+
+      
+    // </div>
+    <div className='flex min-h-[60vh] justify-center items-center'>
+      <LoadingDeye />
     </div>
   );
 }
