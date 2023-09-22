5cb79a27ceb04716f80dbcfa0ad56fb11d9fb603
BrunoDeye <bruno.rodrigues@deye.solar>
2023-09-22 15:59:35 -0300

    test: suspense

--- src/lib/components/layout/Footer.tsx
+++ src/lib/components/layout/Footer.tsx
@@ -7,6 +7,7 @@ import { useEffect } from 'react';
 import { useDataStore } from '@/store/data';
 import { usePathname } from 'next/navigation';
 import { useRouter } from 'next/navigation';
+import { Suspense } from 'react';
 
 const Footer = () => {
   const { theme, setTheme, systemTheme } = useTheme();
@@ -32,23 +33,26 @@ const Footer = () => {
     <footer className="wrapper mt-6">
       <div className="flex">
         <p className="flex flex-col items-center justify-center gap-2 text-xs">
-          {theme === 'light' ? (
-            <Image
-              className=""
-              width={60}
-              height={40}
-              alt="Deye logo"
-              src={logoDeye}
-            />
-          ) : (
-            <Image
-              className=""
-              width={60}
-              height={40}
-              alt="Deye logo"
-              src={logoDeyeWhite}
-            />
-          )}
+          <Suspense fallback={ <span></span>} >
+            {theme === 'light' ? (
+              <Image
+                className=""
+                width={60}
+                height={40}
+                alt="Deye logo"
+                src={logoDeye}
+              />
+            ) : (
+              <Image
+                className=""
+                width={60}
+                height={40}
+                alt="Deye logo"
+                src={logoDeyeWhite}
+              />
+            )}
+          </Suspense>
+
           {new Date().getFullYear()}
         </p>
       </div>
