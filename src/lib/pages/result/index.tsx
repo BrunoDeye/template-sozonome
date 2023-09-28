4fc163d27acea0772b95b5a5b65f5e1df89671e6
BrunoDeye <bruno.rodrigues@deye.solar>
2023-09-28 09:08:52 -0300

    test 4

--- src/lib/pages/result/index.tsx
+++ src/lib/pages/result/index.tsx
@@ -6,7 +6,6 @@ import { dehydrate } from '@tanstack/react-query';
 import { getBatteriesQueryFn } from '@/services/QueryFns/batteryQueryFns';
 import { getAllInOnesQueryFn } from '@/services/QueryFns/allInOneQueryFns';
 import dynamic from 'next/dynamic';
-import Watermark from '@/lib/components/structures/result/Watermark';
 
 const Body = dynamic(() => import('@/lib/components/structures/result/Body'));
 
@@ -21,7 +20,22 @@ const Result: NextPage = async () => {
   return (
     <ReactQueryHydrate state={dehydratedState}>
       <div className="mx-auto flex min-h-[60vh] max-w-3xl flex-col overflow-x-hidden px-6 py-2 max-[300px]:px-1 sm:py-4">
-        <Watermark />
+        <div className="watermark"></div>
+        <div className="watermark !hidden"></div>
+        <div className="watermark"></div>
+        <div className="watermark !hidden"></div>
+        <div className="watermark"></div>
+        <div className="watermark"></div>
+        <div className="watermark !hidden"></div>
+        <div className="watermark"></div>
+        <div className="watermark !hidden"></div>
+        <div className="watermark"></div>
+        <div className="watermark !hidden"></div>
+        <div className="watermark"></div>
+        <div className="watermark !hidden"></div>
+        <div className="watermark"></div>
+        <div className="watermark !hidden"></div>
+        <div className="watermark"></div>
         <Body />
       </div>
     </ReactQueryHydrate>
