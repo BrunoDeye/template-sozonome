e1aab2352b6443c4909e5e9a58e3844449386cb7
BrunoDeye <bruno.rodrigues@deye.solar>
2023-10-11 16:25:30 -0300

    feat: test

--- src/lib/components/layout/index.tsx
+++ src/lib/components/layout/index.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import { type ReactNode } from 'react';
 
 import { ThemeProvider } from '@/lib/components/theme-provider';
@@ -8,34 +8,40 @@ import { ThemeProvider } from '@/lib/components/theme-provider';
 import dynamic from 'next/dynamic';
 import { useLocalStorage } from '@/lib/hooks/useLocalStorage';
 import { useDataStore } from '@/store/data';
-import { usePathname } from 'next/navigation';
-const  Header = dynamic(() => import('./Header'))
-const  Footer = dynamic(() => import('./Footer'), { ssr: false })
+import { motion, AnimatePresence, delay } from 'framer-motion';
+const Header = dynamic(() => import('./Header'));
+const Footer = dynamic(() => import('./Footer'), { ssr: false });
+import { usePathname } from 'next-intl/client';
 
 type LayoutProps = {
   children: ReactNode;
 };
 
 const Layout = ({ children }: LayoutProps) => {
-  
-  return (
+  const pathname = usePathname();
 
+  return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <div className="flex min-h-screen flex-col">
         <Header />
-          <div
+        <AnimatePresence mode="wait">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            key={pathname}
             className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
             aria-hidden="true"
           >
             <div
-              className="relative left-0 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80cc] to-[#ff8080] dark:from-[#80d5ff] dark:to-[#9089fc] dark:opacity-60 opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+              className="relative left-0 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80cc] to-[#ff8080] opacity-30 dark:from-[#80d5ff] dark:to-[#9089fc] dark:opacity-60 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
               style={{
                 clipPath:
                   'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
               }}
             />
-          </div>
-
+          </motion.div>
+        </AnimatePresence>
         <main className="wrapper">{children}</main>
         <Footer />
       </div>
