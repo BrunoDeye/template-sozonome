02757f7b5b8312381863809958f35613e0cc1556
BrunoDeye <bruno.rodrigues@deye.solar>
2023-11-24 11:46:58 -0300

    test3

--- src/app/client/prisma.ts
+++ src/app/client/prisma.ts
@@ -1,10 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma: PrismaClient;
+	var prisma: PrismaClient;
 }
 
-const client = globalThis.prisma || new PrismaClient({ errorFormat: 'pretty' });
-if (process.env.NODE_ENV !== "production" && process.env.NODE_ENV as any !== "preview" ) globalThis.prisma = client;
+const prisma = global.prisma || new PrismaClient({ log: ["info"] });
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
-export default client;
+export default prisma;
